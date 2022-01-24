cfda94f7f47a56159a208182094b4c8e3ee1408e
linlinlinL <50984992+linlinlinL@users.noreply.github.com>
2022-01-24 16:55:57 -0500

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+#!/user/bin/env bash
+
+function ask{
+        echo "How many files are in the current directory?"
+        read input
+    file=$(ls -1 | wc -1)
+}
+
+ask
+
+while [[ $guess -ne $file ]]
+do
+          if [[ $input -lt $file ]]
+          then
+                  echo "The input is smaller than the file number."
+          else
+                  echo "The input is bigger than the file number."
+          fi
+          ask
+done
+
+echo "Congratulations! You have the answer right! "
